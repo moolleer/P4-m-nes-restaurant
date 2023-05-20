0790393a08f3d30e2e5609f4235c66c192775a88
moolleer <m_moller@live.se>
2023-05-20 21:29:32 +0000

    feat: add booking model and register in admin panel

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Booking(models.Model):
+    booking_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False)
+    booked_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user_bookings")
+    date = models.DateField()
+    time = models.TimeField()
+    no_of_guests = models.IntegerField()
+    message = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        return f'Booking {self.date}, {self.time} by {self.booked_by}'
--- mones/settings.py
+++ mones/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -67,7 +66,7 @@ ROOT_URLCONF = 'mones.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
