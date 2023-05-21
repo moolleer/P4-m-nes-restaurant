9d7b7299d3aeb405c4b1828b4d736656ef6372fc
moolleer <m_moller@live.se>
2023-05-21 06:45:07 +0000

    feat: add table model

--- booking/admin.py
+++ booking/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Booking
+from .models import Booking, Table
 
 
 admin.site.register(Booking)
+admin.site.register(Table)
--- booking/models.py
+++ booking/models.py
@@ -3,13 +3,21 @@ import uuid
 from django.contrib.auth.models import User
 
 
+class Table(models.Model):
+    table_number = models.IntegerField()
+    max_no_guests = models.IntegerField()
+
+    def __str__(self):
+        return f'Table {self.table_number}, max guests {self.max_no_guests}'
+
+
 class Booking(models.Model):
     booking_id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False)
     booked_by = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="user_bookings")
-    date = models.DateField()
-    time = models.TimeField()
+    date = models.DateField(null=False)
+    time = models.TimeField(null=False)
     no_of_guests = models.IntegerField()
     message = models.TextField(blank=True)
 
